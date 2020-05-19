a94db4ef4e36e00e5926baf8f57259b34cc57994
episodev20 <34587570+episodev20@users.noreply.github.com>
2020-05-19 16:10:53 +0200

    starting modeling

--- investigation.R
+++ investigation.R
@@ -1,5 +1,6 @@
 load('dogs.RData')
 
+library(class)
 #######################
 # Investigation - CB  #
 #######################
@@ -21,4 +22,20 @@ ddc$NON_DOG_OWNERS <- ddc$TOTAL_POPULATION - ddc$TOTAL_UNIQUE_OWNERS
 ddc$PERCENT_DOG_OWNERS <- ddc$TOTAL_UNIQUE_OWNERS / ddc$TOTAL_POPULATION
 
 
+# k nearest neighbours template
 
+#https://www.edureka.co/blog/knn-algorithm-in-r/
+#https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/factor
+# for labeling for using text in model.
+
+#data separation
+dat.d <- sample(1:nrow(loan.subset.n),size=nrow(loan.subset.n)*0.7,replace = FALSE) #random selection of 70% data.
+
+train.loan <- loan.subset[dat.d,] # 70% training data
+test.loan <- loan.subset[-dat.d,] # remaining 30% test data
+
+
+#aprox k value
+k <-  sqrt(nrow(dogs2020))
+
+knn.test <- knn(train=train.loan, test=test.loan, cl=train.loan_labels, k=26)
