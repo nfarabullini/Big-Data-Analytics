0e80af68e2c5883c0c1c2f644186ab521711fa52
Nicoletta Farabullini <nicoletta.farabullini@mirai-solutions.com>
2020-05-20 20:36:08 +0200

    initial comparison in data set among entries

--- gender_comparison.R
+++ gender_comparison.R
@@ -2,6 +2,7 @@ library(readr)
 library(data.table)
 library(xlsx)
 library(dplyr)
+library(ggplot2)
 
 # this removes all variables, usefull if we rerun code to keep it clean
 rm(list=ls())
@@ -25,15 +26,33 @@ dogs2020 <- na.omit(dogs2020)
 setnames(dogs2020, old = c("HALTER_ID", "ALTER", "GESCHLECHT", "STADTQUARTIER", "RASSE1", "RASSENTYP", "GEBURTSJAHR_HUND", "GESCHLECHT_HUND", "HUNDEFARBE")
          , new = c("OWNER_ID", "AGE", "SEX", "DISTRICT", "BREED", "BREEDTYPE", "YOB_DOG", "SEX_DOG", "COLOR_DOG"))
 
-library(ggplot2)
+# filter the data frame for the most common breeds
+unique_breeds <- unique(dogs2020$BREED)
+entry_breed_num <- lapply(unique_breeds, function(x) {
+  length(which(x == dogs2020$BREED))
+})
+filtered_breeds <- dogs2020$BREED[which(entry_breed_num > 100)]
+
+# age group that owns the most dogs
+unique_ages <- unique(dogs2020$AGE)
+entry_aged_num <- lapply(unique_ages, function(x) {
+  length(which(x == dogs2020$AGE))
+})
+max_age_group <- dogs2020$AGE[max(unlist(entry_aged_num))]
+
+dogs_filtered <- dogs2020 %>% filter(BREED == unique(filtered_breeds), AGE == max_age_group)
+# bar plot for highest age group owners and most popular breeds
+ggplot(dogs_filtered, aes(dogs_filtered$BREED)) + geom_bar()
+
+# do analysis for: BREED VS:
+# color_dog, district_name, income (T. CHF), Basic_school %, Gymnasium %, University %, wealth (T. CHF), Small buildings
+
 # gender-based stats
 men <- 100*(nrow(dogs2020 %>% filter(dogs2020$SEX == "m"))/nrow(dogs2020))
 women <- 100*(nrow(dogs2020 %>% filter(dogs2020$SEX == "w"))/nrow(dogs2020))
 data.frame("Gender" = c("Male", "Female"), "Percentage %" = c(men, women))
 
 # create histos for most popular breeds
-women_owners <- owners_gender("w") 
-men_owners <- owners_gender("m") 
 owners_gender <- function(param) {
   owners <- dogs2020 %>% filter(dogs2020$SEX == param) 
   breeds <- unique(dogs2020$BREED)
@@ -46,5 +65,9 @@ owners_gender <- function(param) {
   owners_70 <- owners %>% filter(owners$BREED %in% unique(owners_70_unfiltered$BREED))
   ggplot(owners_70, aes(owners_70$BREED)) + geom_bar()
 }
+
+women_owners <- owners_gender("w") 
+men_owners <- owners_gender("m") 
+
 
 
